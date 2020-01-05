6c2edff9913111754122b1dc87d3e60335b32ac8
Harrison <hjbwallace@gmail.com>
2020-01-05 21:02:30 +0800

    Adding the ConsoleApp that all applications should inherit from
    * Example app to show the framework in action

---
+++ src/SimpleConsole.ExampleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace SimpleConsole.ExampleApp
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await new ConsoleApp().RunAsync();
+        }
+    }
+}
---
+++ src/SimpleConsole/ConsoleApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleConsole
+{
+    public class ConsoleApp
+    {
+        private bool _isRunning;
+
+        public async Task RunAsync()
+        {
+            _isRunning = true;
+
+            Console.WriteLine("Welcome to a console app!");
+
+            while (_isRunning)
+            {
+                Console.WriteLine("Waiting for imput (or empty to exit): ");
+                var input = Console.ReadLine();
+
+                if (input == string.Empty)
+                    _isRunning = false;
+
+                await Task.CompletedTask;
+            }
+
+            Console.WriteLine("Closing!");
+        }
+    }
+}
