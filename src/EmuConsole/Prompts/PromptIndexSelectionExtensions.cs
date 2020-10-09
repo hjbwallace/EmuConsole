354a83331a1527bd972273b27dd0937f90eaf10b
Harrison Wallace <hjbwallace@gmail.com>
2020-10-09 23:07:38 +0800

    Adding Inline logic to the index prompt extensions

--- src/EmuConsole/Prompts/PromptIndexSelectionExtensions.cs
+++ src/EmuConsole/Prompts/PromptIndexSelectionExtensions.cs
@@ -5,28 +5,28 @@ namespace EmuConsole
 {
     public static class PromptIndexSelectionExtensions
     {
-        public static T PromptIndexSelection<T>(this IConsole console, IEnumerable<T> source)
+        public static T PromptIndexSelection<T>(this IConsole console, IEnumerable<T> source, bool writeInline = false)
         {
             var indexCollection = new IndexCollection<T>(source);
-            return indexCollection.GetSelection(console);
+            return indexCollection.GetSelection(console, writeInline);
         }
 
-        public static T PromptIndexSelection<T>(this IConsole console, IEnumerable<T> source, Func<T, object> descriptionSelector)
+        public static T PromptIndexSelection<T>(this IConsole console, IEnumerable<T> source, Func<T, object> descriptionSelector, bool writeInline = false)
         {
             var indexCollection = new IndexCollection<T>(source, descriptionSelector);
-            return indexCollection.GetSelection(console);
+            return indexCollection.GetSelection(console, writeInline);
         }
 
-        public static T[] PromptIndexSelections<T>(this IConsole console, IEnumerable<T> source, bool allowEmpty = false)
+        public static T[] PromptIndexSelections<T>(this IConsole console, IEnumerable<T> source, bool allowEmpty = false, bool writeInline = false)
         {
             var indexCollection = new MultipleIndexCollection<T>(source, allowEmpty);
-            return indexCollection.GetSelection(console);
+            return indexCollection.GetSelection(console, writeInline);
         }
 
-        public static T[] PromptIndexSelections<T>(this IConsole console, IEnumerable<T> source, Func<T, object> descriptionSelector, bool allowEmpty = false)
+        public static T[] PromptIndexSelections<T>(this IConsole console, IEnumerable<T> source, Func<T, object> descriptionSelector, bool allowEmpty = false, bool writeInline = false)
         {
             var indexCollection = new MultipleIndexCollection<T>(source, descriptionSelector, allowEmpty);
-            return indexCollection.GetSelection(console);
+            return indexCollection.GetSelection(console, writeInline);
         }
     }
 }
