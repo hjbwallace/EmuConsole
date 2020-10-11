54c8c816c3a7a505d7c8c3ff2a24880c355ee43d
Harrison Wallace <hjbwallace@gmail.com>
2020-10-11 22:20:43 +0800

    Adding enum prompt shortcuts
    * Allows selecting an enum value via an index

---
+++ src/EmuConsole.Tests/Prompts/PromptEnumTests.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace EmuConsole.Tests.Prompts
+{
+    public enum TestEnum
+    {
+        Value1,
+        Value2,
+    }
+
+    public class PromptEnumTests : ConsoleTestBase
+    {
+        [Fact]
+        public void CanSelectEntryFromEnum()
+        {
+            _console.AddLinesToRead(20, 3, 0);
+            var result = _console.PromptEnumIndexSelection<TestEnum>(false);
+
+            Assert.Equal(TestEnum.Value1, result);
+
+            _console.HasLinesRead(3);
+            _console.HasLinesWritten(3);
+            _console.HasOutput($@"
+[0] Value1
+[1] Value2
+> 20
+> 3
+> 0
+");
+        }
+
+        [Fact]
+        public void CanSelectEntryFromEnumWithInline()
+        {
+            _console.AddLinesToRead(20, 3, 0);
+            var result = _console.PromptEnumIndexSelection<TestEnum>(true);
+
+            Assert.Equal(TestEnum.Value1, result);
+
+            _console.HasLinesRead(3);
+            _console.HasLinesWritten(2);
+            _console.HasOutput($@"
+[0] Value1 [1] Value2 
+> 20
+> 3
+> 0
+");
+        }
+    }
+}
---
+++ src/EmuConsole/Prompts/PromptEnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace EmuConsole
+{
+    public static class PromptEnumExtensions
+    {
+        public static TEnum PromptEnumIndexSelection<TEnum>(this IConsole console, bool writeInline = false) where TEnum : Enum
+        {
+            var source = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+            return console.PromptIndexSelection(source, writeInline);
+        }
+
+        public static TEnum[] PromptEnumIndexSelections<TEnum>(this IConsole console, bool allowEmpty = false, bool writeInline = false) where TEnum : Enum
+        {
+            var source = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+            return console.PromptIndexSelections(source, allowEmpty, writeInline);
+        }
+    }
+}
