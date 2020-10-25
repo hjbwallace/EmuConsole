3b220f2fe57b48885b6c255e9f1e40a60b82ed96
Harrison Wallace <hjbwallace@gmail.com>
2020-10-25 16:40:46 +0800

    MultipleIndexCollection should inherit from MultipleInputCollection to consolidate logic

--- src/EmuConsole/Collections/MultipleIndexCollection.cs
+++ src/EmuConsole/Collections/MultipleIndexCollection.cs
@@ -1,81 +1,56 @@
-﻿using EmuConsole.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace EmuConsole
 {
-    public class MultipleIndexCollection<TEntity>
+    public class MultipleIndexCollection<TEntity> : MultipleInputCollection<int, TEntity>
     {
-        private readonly IList<KeyValuePair<string, TEntity>> _source;
-        private readonly Func<TEntity, object> _descriptionSelector;
-        private readonly bool _allowEmpty;
+        public MultipleIndexCollection(IDictionary<int, TEntity> source,
+                                       Func<int, TEntity, object> descriptionSelector = null,
+                                       bool allowEmpty = false) 
+            : base(source, descriptionSelector, allowEmpty)
+        {
+        }
+
+        public MultipleIndexCollection(IEnumerable<TEntity> source,
+                                       Func<int, TEntity, object> descriptionSelector = null,
+                                       bool allowEmpty = false) 
+            : this(MapSourceFromEnumerable(source), descriptionSelector, allowEmpty)
+        {
+        }
 
         public MultipleIndexCollection(IEnumerable<TEntity> source, bool allowEmpty)
             : this(source, x => x?.ToString(), allowEmpty)
         {
         }
 
-        public MultipleIndexCollection(IEnumerable<TEntity> source, Func<TEntity, object> descriptionSelector, bool allowEmpty)
+        public MultipleIndexCollection(IEnumerable<TEntity> source,
+                                       Func<TEntity, object> descriptionSelector,
+                                       bool allowEmpty)
+            : this(MapSourceFromEnumerable(source), GenerateDescriptionSelector(descriptionSelector), allowEmpty)
         {
-            if (!source.Any())
-                throw new ArgumentException("Source must be populated");
-
-            _source = source.Select((x, i) => new KeyValuePair<string, TEntity>(i.ToString(), x)).ToList();
-            _descriptionSelector = descriptionSelector;
-            _allowEmpty = allowEmpty;
         }
 
-        public TEntity[] GetSelection(IConsole console, CollectionWriteStyle style)
+        protected override KeyValuePair<int, TEntity>[] MapSource(IEnumerable<KeyValuePair<int, TEntity>> source)
         {
-            return GetSelectionInternal(console, style, _source, true);
-        }
-
-        private IList<KeyValuePair<string, string>> GenerateDisplay(IEnumerable<KeyValuePair<string, TEntity>> source)
-        {
-            var length = source.Count();
-            var padSize = length.ToString().Length;
-
-            return source.Select(x => new KeyValuePair<string, string>(
-                x.Key?.PadLeft(padSize),
-                _descriptionSelector(x.Value).ToString()))
-                .ToList();
-        }
-
-        private TEntity[] GetSelectionInternal(IConsole console, CollectionWriteStyle style, IEnumerable<KeyValuePair<string, TEntity>> source, bool writeCollection)
-        {
-            if (writeCollection)
-            {
-                var display = GenerateDisplay(source);
-
-                console.WriteLine();
-                console.WriteCollection(display, style);
-            }
-
-            var inputs = console.PromptInputs(null, _allowEmpty);
-
-            if (!inputs.Any())
-                return new TEntity[0];
-
-            if (inputs[0]?.StartsWith("%") == true)
-            {
-                var filter = inputs[0].Substring(1).Trim();
-                var newSource = _source
-                    .Where(x => _descriptionSelector(x.Value)?.ToString().Contains(filter, StringComparison.InvariantCultureIgnoreCase) == true)
-                    .Select((x, i) => new KeyValuePair<string, TEntity>(i.ToString(), x.Value))
-                    .ToArray();
-
-                return GetSelectionInternal(console, style, newSource.Any() ? newSource : _source, true);
-            }
-
-            var foundInputs = inputs.Intersect(source.Select(x => x.Key));
-
-            if (!foundInputs.Any() && !_allowEmpty)
-                return GetSelectionInternal(console, style, source, false);
-
-            return foundInputs
-                .Select(x => source.Single(a => a.Key == x).Value)
+            return source
+                .Select((x, i) => new KeyValuePair<int, TEntity>(i, x.Value))
                 .ToArray();
+        }
+
+        private static Func<int, TEntity, object> GenerateDescriptionSelector(Func<TEntity, object> descriptionSelector)
+        {
+            return descriptionSelector != null
+                ? ((key, value) => descriptionSelector(value))
+                : (Func<int, TEntity, object>)null;
+        }
+
+        private static IDictionary<int, TEntity> MapSourceFromEnumerable(IEnumerable<TEntity> source)
+        {
+            return source?
+                .Select((x, i) => new KeyValuePair<int, TEntity>(i, x))
+                .ToDictionary(x => x.Key, x => x.Value);
         }
     }
 }
